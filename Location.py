74a81e8d5dd2b706953a511f664056e30c945fa0
arabene <jeongy414@naver.com>
2025-06-13 03:26:07 +0000

    Update app layout

---
+++ Location.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from streamlit_folium import folium_static
+import folium
+from folium.plugins import MarkerCluster
+import pandas as pd
+
+def app():
+    st.title("진주시 CCTV 현황")
+
+    df = pd.read_csv("/workspaces/datascience1/charger_20230531.csv", encoding='euc-kr')
+
+    st.dataframe(df, height=200)
+
+    df[["lat","lon"]] = df[["위도","경도"]]
+
+    m = folium.Map(location=[35.1799817, 128.1076213], zoom_start=13)
+
+    marker_cluster = MarkerCluster().add_to(m)
+
+    for idx, row in df.iterrows():
+        folium.Marker(
+            location=[row["lat"], row["lon"]],
+            popup=row["설치장소"],
+            icon=folium.Icon(color="blue", icon="info-sign"),
+        ).add_to(marker_cluster)
+
+    folium_static(m)
