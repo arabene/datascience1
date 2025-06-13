1886a397847aaadc995d4ae293a00a6a4864190a
arabene <jeongy414@naver.com>
2025-06-13 03:42:54 +0000

    6

--- Location.py
+++ Location.py
@@ -1,14 +1,12 @@
-import streamlit as st
-from streamlit_folium import folium_static
-import folium
-from folium.plugins import MarkerCluster
-import pandas as pd
 import os
+import streamlit as st
+import pandas as pd
 
 def app():
     st.title("전기차 충전소 현황")
 
-    # CSV 읽기
-    df = pd.read_csv("charger_20230531.csv", encoding='utf-8')
+    st.write("현재 작업 경로:", os.getcwd())
+    st.write("현재 디렉터리 파일 목록:", os.listdir())
 
+    df = pd.read_csv("charger_20230531.csv", encoding='utf-8')
     st.write(df.head())
