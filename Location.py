ccb0ec63f034d588fbbbc47e1634ba7559bdbb40
arabene <jeongy414@naver.com>
2025-06-13 03:42:05 +0000

    5

--- Location.py
+++ Location.py
@@ -8,11 +8,7 @@ import os
 def app():
     st.title("전기차 충전소 현황")
 
-    # 현재 작업 디렉토리와 파일 목록 출력(디버깅용)
-    st.write("현재 작업 경로:", os.getcwd())
-    st.write("현재 디렉토리 파일 목록:", os.listdir())
-
     # CSV 읽기
-    df = pd.read_csv("charger_20230531.csv", encoding='euc-kr')
+    df = pd.read_csv("charger_20230531.csv", encoding='utf-8')
 
     st.write(df.head())
