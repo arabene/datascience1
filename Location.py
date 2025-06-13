edad3b270c559d39b81f4fd25762f58d9c611b26
arabene <jeongy414@naver.com>
2025-06-13 03:48:26 +0000

    map

--- Location.py
+++ Location.py
@@ -1,12 +1,27 @@
-import os
 import streamlit as st
+from streamlit_folium import folium_static
+import folium
+from folium.plugins import MarkerCluster
 import pandas as pd
 
-def app():
-    st.title("전기차 충전소 현황")
 
-    st.write("현재 작업 경로:", os.getcwd())
-    st.write("현재 디렉터리 파일 목록:", os.listdir())
+st.title("전기차 충전소 현황")
 
-    df = pd.read_csv("/mount/src/datascience1/charger_20230531.csv", encoding='cp949')
-    st.write(df.head())
+df = pd.read_csv("/mount/src/datascience1/charger_20230531.csv", encoding='euc-kr')
+
+st.dataframe(df, height=200)
+
+df[["lat","lon"]] = df[["위도","경도"]]
+
+m = folium.Map(location=[35.1799817, 128.1076213], zoom_start=13)
+
+marker_cluster = MarkerCluster().add_to(m)
+
+for idx, row in df.iterrows():
+    folium.Marker(
+        location=[row["lat"], row["lon"]],
+        popup=row["설치장소"],
+        icon=folium.Icon(color="blue", icon="info-sign"),
+    ).add_to(marker_cluster)
+
+folium_static(m)
